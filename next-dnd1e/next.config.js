e1a33118b3554d486f5be97810a5b66a1dfa0cbb
Vaernion <vaporz@gmail.com>
2022-04-24 21:30:58 +0200

    analyze bundle size

--- next-dnd1e/next.config.js
+++ next-dnd1e/next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+// module.exports = nextConfig;
+
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
+module.exports = withBundleAnalyzer({
+  // This is just your regular next.config.js options. For example:
+  ...nextConfig,
+});
